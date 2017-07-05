8dd69af7ce59b536f3af91b59b030d5d365f3c93
topdude15 <topdude15@gmail.com>
2017-07-05 16:58:38 -0400

    Error with getting group name

---
+++ ChatRoom/Group.swift
@@ -0,0 +1,9 @@
+//
+//  Group.swift
+//  ChatRoom
+//
+//  Created by Trevor Rose on 7/3/17.
+//  Copyright © 2017 Trevor Rose. All rights reserved.
+//
+
+import Foundation
---
+++ ChatRoom/GroupCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroupCell.swift
+//  ChatRoom
+//
+//  Created by Trevor Rose on 7/5/17.
+//  Copyright © 2017 Trevor Rose. All rights reserved.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
